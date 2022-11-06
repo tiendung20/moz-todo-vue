cd8ccf83d98eafa7b923dfefba938f54448b5fdd
tiendung20 <ntdung2316@gmail.com>
2022-11-06 17:42:56 +0700

    Add Loop ToDoItem

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <script>
 import ToDoItem from "./components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
 
 export default {
   name: "app",
   components: {
     ToDoItem,
+  },
+  data: function () {
+    return {
+      ToDoItems: [
+        { id: uniqueId("todo-"), label: "Learn Vue", done: false },
+        {
+          id: uniqueId("todo-"),
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        { id: uniqueId("todo-"), label: "Have fun", done: true },
+        { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
+      ],
+    };
   },
 };
 </script>
@@ -13,8 +28,12 @@ export default {
   <div id="app">
     <h1>To-Do List</h1>
     <ul>
-      <li>
-        <to-do-item label="My ToDo Item" :done="true"></to-do-item>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item
+          :label="item.label"
+          :done="item.done"
+          :id="item.id"
+        ></to-do-item>
       </li>
     </ul>
   </div>
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,15 +1,13 @@
 <script>
-import uniqueId from "lodash.uniqueid";
-
 export default {
   props: {
     label: { required: true, type: String },
     done: { default: false, type: Boolean },
+    id: { required: true, type: String },
   },
   data: function () {
     return {
       isDone: this.done,
-      id: uniqueId("todo-"),
     };
   },
 };
