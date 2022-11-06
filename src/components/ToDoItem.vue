60fb4074e67541db1bf5c08d9ef0525e7bac3712
tiendung20 <ntdung2316@gmail.com>
2022-11-06 23:55:14 +0700

    Add ToDoItemEditForm

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,5 +1,10 @@
 <script>
+import ToDoItemEditForm from "./ToDoItemEditForm.vue";
+
 export default {
+  components: {
+    ToDoItemEditForm,
+  },
   props: {
     label: { required: true, type: String },
     done: { default: false, type: Boolean },
@@ -7,23 +12,60 @@ export default {
   },
   data: function () {
     return {
-      isDone: this.done,
+      isEditing: false,
     };
+  },
+  methods: {
+    deleteToDo() {
+      this.$emit("item-deleted");
+    },
+    toggleToItemEditForm() {
+      this.isEditing = true;
+    },
+    itemEdited(newLabel) {
+      this.$emit("item-edited", newLabel);
+      this.isEditing = false;
+    },
+    editCancelled() {
+      this.isEditing = false;
+    },
+  },
+  computed: {
+    isDone() {
+      return this.done;
+    },
   },
 };
 </script>
 
 <template>
-  <div class="custom-checkbox">
-    <input
-      type="checkbox"
-      :id="id"
-      :checked="isDone"
-      @change="$emit('checkbox-changed')"
-      class="checkbox"
-    />
-    <label :for="id" class="checkbox-label">{{ label }}</label>
+  <div class="stack-small" v-if="!isEditing">
+    <div class="custom-checkbox">
+      <input
+        type="checkbox"
+        class="checkbox"
+        :id="id"
+        :checked="isDone"
+        @change="$emit('checkbox-changed')"
+      />
+      <label :for="id" class="checkbox-label">{{ label }}</label>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn" @click="toggleToItemEditForm">
+        Edit <span class="visually-hidden">{{ label }}</span>
+      </button>
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete <span class="visually-hidden">{{ label }}</span>
+      </button>
+    </div>
   </div>
+  <to-do-item-edit-form
+    v-else
+    :label="label"
+    :id="id"
+    @item-edited="itemEdited"
+    @edit-cancelled="editCancelled"
+  ></to-do-item-edit-form>
 </template>
 
 <style scoped>
