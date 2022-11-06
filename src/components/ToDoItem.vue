164f564dde5906a0a25d529f896a533220977ddf
tiendung20 <ntdung2316@gmail.com>
2022-11-06 17:10:35 +0700

    Add ToDoItem

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,23 @@
+<script>
+import uniqueId from "lodash.uniqueid";
+
+export default {
+  props: {
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean },
+  },
+  data: function () {
+    return {
+      isDone: this.done,
+      id: uniqueId("todo-"),
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <input type="checkbox" :id="id" :checked="isDone" />
+    <label for="todo-item">{{ label }}</label>
+  </div>
+</template>
